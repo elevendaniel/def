9d2fde0092641293e9e7a7b92d2cc08368f4e7a1
danielliu <danielliu@DanieldeMacBook-Pro.local>
2019-02-27 22:38:25 +0800

    def clasic

---
+++ def.py
@@ -0,0 +1,37 @@
+#读取部分
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+#转换部分
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	return new
+
+#写文档部分
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+#触发机制 触发点
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+
+#接入点
+main()
